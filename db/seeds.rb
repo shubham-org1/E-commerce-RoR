3d30592c7fdb690dd720666953c0b4ac1e688cab
Roberto Piancatelli <piancatellirob@gmail.com>
2021-04-14 11:28:52 +0100

    added view page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "open-uri"
+require "faker"
+
+puts "Running seed..."
+puts "Destorying tables..."
+
+Product.destroy_all
+Category.destroy_all
+
+product_name1 = "Soap"
+product_name2 = "Shampoo"
+
+product_name_arr = [product_name1, product_name2]
+
+category1 = "Skincare"
+
+category_arr = [category1]
+
+puts "Creating products...."
+
+2.times do |product|
+     product = Product.new(
+         name: Faker::Commerce.product_name,
+         description: "Nice description" || "Not a good description",
+         price: rand(1..10)
+     )
+     product.save!
+end
+
+
